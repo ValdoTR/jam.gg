43deda18c7126bb22794877af7d0f2ca92ddf769
Valdo <vteixeiraromao@gmail.com>
2022-07-02 17:11:58 +0100

    Added nes.css style for button

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
+import { CreateUIWebsiteEvent } from "@workadventure/iframe-api-typings/Api/Events/ui/UIWebsite";
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 console.log('Script started successfully');
@@ -10,6 +11,31 @@ WA.onInit().then(() => {
     console.log('Player tags: ',WA.player.tags)
 
     console.log('playerId',WA.player.id)
+    
+    WA.controls.disablePlayerProximityMeeting()
+
+    let letsPlayButton: CreateUIWebsiteEvent = {
+        url: "http://localhost:3000/src/letsPlayButton.html", // Dev Webiste URL
+        //url: "https://valdotr.github.io/jam.gg/src/letsPlayButton.html", // Website URL
+        visible: true,     // The website is visible or not
+        allowApi: true,    // Allow scripting API on the website
+        allowPolicy: "",   // The list of feature policies allowed
+        position: {
+            vertical: "top",
+            horizontal: "middle",
+        },
+        size: {            // Size on the UI (available units: px|em|%|cm|in|pc|pt|mm|ex|vw|vh|rem and others values auto|inherit)
+            height: "200px",
+            width: "400px",
+        },
+        margin: {          // Website margin (available units: px|em|%|cm|in|pc|pt|mm|ex|vw|vh|rem and others values auto|inherit)
+            top: "5px",
+            bottom: "5px",
+            left: "5px",
+            right: "5px",
+        },
+    }
+    WA.ui.website.open(letsPlayButton)
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
